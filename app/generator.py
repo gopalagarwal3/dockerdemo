3a30ffc6ab467b495ae772cc206b2b9047c459f1
Gopal Agarwal <gagarwal@spglobal.com>
2019-04-22 16:56:44 +0530

    Commit to break the tests

--- app/generator.py
+++ app/generator.py
@@ -10,7 +10,6 @@ adverbs = ('remarkably', 'enormously', 'substantially', 'significantly')
 
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
-
 def sample(l, n=1):
     result = random.sample(l, n)
     if n == 1:
@@ -21,13 +20,13 @@ def sample(l, n=1):
 def generate_buzz():
 
     buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-        sample(verbs), buzz_terms[1]])
-    return 'V1: ' + phrase.title()
+    # phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+    #     sample(verbs), buzz_terms[1]])
+    # return 'V1: ' + phrase.title()
 
     # Breaking Tests
-    # phrase = ' '.join([sample(adjectives), buzz_terms[0]])
-    # return 'V2: ' + phrase.title()
+    phrase = ' '.join([sample(adjectives), buzz_terms[0]])
+    return 'V2: ' + phrase.title()
 
 
 if __name__ == "__main__":
